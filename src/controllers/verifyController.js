cf7e8ec50a000f94f3db28d4905e0f675d1e1fcb
Micael Santos <micaelmaicon@hotmail.com>
2022-12-04 19:19:07 -0300

    feat: finish verify password

--- src/controllers/verifyController.js
+++ src/controllers/verifyController.js
@@ -1,19 +1,10 @@
 const service = require('../services/verifyService');
 
-const verifyPassword = async (req, res) => {
-  console.log('Cheguei no controller');
+const verifyPassword = (req, res) => {
   const { password, rules } = req.body;
-  const { status, verify, noMatch } = await service.verify(password, rules);
-  if (verify) res.status(status).json({ verify, noMatch });
-  // return res.status(200).send(resultMatch);
-
-  // try {
-  //   const { status, message, resultLogin } = await service.login(username, password);
-  //   if (message) return res.status(status).json({ message });
-  //   return res.status(status).json(resultLogin);
-  // } catch (err) {
-  //   return res.status(SERVER_ERROR).json(SERVER_ERROR_MESSAGE);
-  // }
+  const { status, noMatch } = service.verifyPassword(password, rules);
+  if (noMatch.length === 0) return res.status(status).json({ verify: true, noMatch });
+  return res.status(status).json({ verify: false, noMatch });
 };
 
 module.exports = { verifyPassword };
--- src/services/verifyService.js
+++ src/services/verifyService.js
@@ -1,18 +1,61 @@
-const verify = (password, _rules) => {
-  // validação de caracter repetido
-  const regexNoRepeat = /([a-z])\1/i;
-  const validateNoRepeat = password.match(regexNoRepeat);
-
-  const regexPassword = /^(?=.*[!@#$%^&*()-+\\/\\{\\}\\[\]])(?=.*[0-9])(?=.*[a-z]).{8,}$/i;
-  const validatePassword = password.match(regexPassword);
-
-  if (validateNoRepeat === null && validatePassword) {
-    console.log('A senha passou');
-    return { status: 200, verify: true, noMatch: [] };
-  }
-
-  return { status: 401, verify: true, noMatch: [] };
-  // ======== Finalizo Senha correta ====
+const regexRules = {
+  regexNoRepeat: /([a-z])\1/gi,
+  regexMinUppercase: /[a-z0-9!@#$%^&*()-+\\/\\{\\}\\[\]]/g,
+  regexMinLowercase: /[A-Z0-9!@#$%^&*()-+\\/\\{\\}\\[\]]/g,
+  regexMinDigit: /[A-Za-z!@#$%^&*()-+\\/\\{\\}\\[\]]/g,
+  regexMinSpecialChars: /[a-z0-9]/gi,
 };
 
-module.exports = { verify };
+const verifyNoRepeat = (payload, rule, value, match) => {
+  const resultMatch = payload.match(regexRules.regexNoRepeat);
+  if (resultMatch && resultMatch.length > value) match.push(rule);
+};
+
+const minSpecialChars = (payload, rule, value, match) => {
+  const newString = payload.replace(regexRules.regexMinSpecialChars, '');
+  if (newString.length < value) match.push(rule);
+};
+
+const verifyMinDigit = (payload, rule, value, match) => {
+  const newString = payload.replace(regexRules.regexMinDigit, '');
+  if (newString.length < value) match.push(rule);
+};
+
+const verifyMinLowercase = (payload, rule, value, match) => {
+  const newString = payload.replace(regexRules.regexMinLowercase, '');
+  if (newString.length < value) match.push(rule);
+};
+
+const verifyMinUppercase = (payload, rule, value, match) => {
+  const newString = payload.replace(regexRules.regexMinUppercase, '');
+  if (newString.length < value) match.push(rule);
+};
+
+const verifyMinSize = (payload, rule, value, match) => {
+  if (payload.length < value) match.push(rule);
+};
+
+const verify = {
+  minSize(payload, rule, value, match) { verifyMinSize(payload, rule, value, match); },
+  minUppercase(payload, rule, value, match) { verifyMinUppercase(payload, rule, value, match); },
+  minLowercase(payload, rule, value, match) { verifyMinLowercase(payload, rule, value, match); },
+  minDigit(payload, rule, value, match) { verifyMinDigit(payload, rule, value, match); },
+  minSpecialChars(payload, rule, value, match) { minSpecialChars(payload, rule, value, match); },
+  noRepeted(payload, rule, value, match) { verifyNoRepeat(payload, rule, value, match); },
+};
+
+const verifyPassword = (password, rules) => {
+  const noMatch = [];
+
+  for (let i = 0; i < rules.length; i += 1) {
+    const { rule, value } = rules[i];
+
+    verify[rule](password, rule, value, noMatch);
+  }
+
+  return { status: 200, noMatch };
+};
+
+module.exports = {
+  verifyPassword,
+};
